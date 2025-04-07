e0bc88bc0fec766cd83aa679e5028ba2ad78ff68
LeoDe0s <orue.leo444@gmail.com>
2025-04-06 22:47:39 -0300

    carpeta con codigos de c#

---
+++ estructura de datos/Carrera de autos/Carrera de autos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+//DISEÑAR UN ALGORITMO QUE SIMULE UNA CARRERA DE AUTOS DONDE CADA AUTO TIENE UNA ACELERACION Y UNA VELOCIDAD MAXIMA.
+//CALCULAR QUIEN GANA UNA CARRERA DE 4KM.
+namespace Carrera_de_autos
+{
+    internal class Program
+    {
+        struct auto
+        {
+            public double a;
+            public double v;
+            public double d;
+            public double vm;
+        }
+
+        static void calcular(ref auto c, int t)
+        {
+            c.v = c.a * t; // velocidad = aceleracion x tiempo
+            if (c.v > c.vm) c.v = c.vm; // si la velocidad es mayor a la velocidad maxima la velocidad maxima pasa a ser la velocidad
+            c.d = c.v * t; // distancia = velocidad x tiempo
+        }
+
+        static void Main(string[] args)
+        {
+            auto[] piloto = new auto[2];
+            piloto[0].a = 1;
+            piloto[0].v = 0;
+            piloto[0].d = 0;
+            piloto[0].vm = 280;
+
+            piloto[1].a = 2;
+            piloto[1].v = 0;
+            piloto[1].d = 0;
+            piloto[1].vm = 300;
+            int t = 0;
+
+            do
+            {
+                calcular(ref piloto[0], t);
+                calcular(ref piloto[1], t);
+                t++;
+            }
+            while (piloto[0].d < 4000 && piloto[1].d < 4000);
+
+            if (piloto[0].d >= 4000 && piloto[1].d >= 4000)
+                Console.WriteLine("Empate");
+
+            else if (piloto[0].d >= 4000)
+                Console.WriteLine("El ganador es el piloto 1");
+
+            else Console.WriteLine("El ganador es el piloto 2");
+
+
+        }
+    }
+}
---
+++ estructura de datos/Nota de 10 alumnos/Nota de 10 alumnos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+//CARGAR EL NOMBRE Y LA NOTA DE 10 ALUMNOS
+namespace Nota_de_10_alumnos
+{
+    internal class Program
+    {
+        struct persona {
+
+            public string nombre;
+            public int nota;
+    
+        }
+
+        static void Main(string[] args)
+        {
+
+            persona[] alumnos = new persona[2];
+            int cont = 0;
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine($"ingrese el nombre del alumno {i + 1}:");
+                alumnos[cont].nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese la nota del alumno:");
+                while (!int.TryParse(Console.ReadLine(), out alumnos[cont].nota))
+                {
+                    Console.WriteLine("Error, ingrese una nota");
+                }
+                i++;
+                cont++;
+            }
+            while (cont < 2);
+
+            Console.WriteLine("notas de los alumnos:");
+            for (int j = 0; j < alumnos.Length; j++)
+            {
+                Console.WriteLine($"Alumno {j + 1}: {alumnos[j].nombre}, Nota: {alumnos[j].nota}");
+            }
+
+        }
+
+         
+    }
+}
+
+
